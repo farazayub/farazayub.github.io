ffeefe6b53207a2f2fda9ad6b6e3c5c8261e664b
farazayub <77578758+farazayub@users.noreply.github.com>
2022-11-05 20:17:58 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Navbar from './components/navbar';
-import AboutMe from './components/aboutMe';
-import Home from './components/home'
-import ContactMe from './components/contactMe';
-
+import "./App.css";
+import {Routes, Route, HashRouter } from "react-router-dom";
+import Navbar from "./components/navbar";
+import AboutMe from "./components/aboutMe";
+import Home from "./components/home";
+import ContactMe from "./components/contactMe";
 
 function App() {
-  
   return (
-   
-   <BrowserRouter>
-   <Navbar/>
-   <Routes>
-       <Route path='/' element={<Home/>}/>
-       <Route path='/AboutMe' element={<AboutMe/>}/>
-       <Route path='/ContactMe' element={<ContactMe/>}/>
-   </Routes>
-   </BrowserRouter>
-   
+    //  <BrowserRouter>
+    //  <Navbar/>
+    //  <Routes>
+    //      <Route path='/' element={<Home/>}/>
+    //      <Route path='/AboutMe' element={<AboutMe/>}/>
+    //      <Route path='/ContactMe' element={<ContactMe/>}/>
+    //  </Routes>
+    //  </BrowserRouter>
 
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AboutMe" element={<AboutMe />} />
+        <Route path="/ContactMe" element={<ContactMe />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
